168162594657d00770dcc8c801745a7d08727351
Phong Nguyen <nguyendoanphong.it@gmail.com>
2024-09-28 16:37:06 +0700

    Html -> Pdf

---
+++ src/HtmlToPdf/PlaywrightConverter/Program.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Playwright;
+
+var httpClient = new HttpClient();
+var response = await httpClient.GetAsync("https://github.com/phongnguyend");
+
+var html = await response.Content.ReadAsStringAsync();
+
+Microsoft.Playwright.Program.Main(["install"]);
+
+using var playwright = await Playwright.CreateAsync();
+
+var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
+{
+    Headless = true,
+});
+
+var page = await browser.NewPageAsync();
+await page.SetContentAsync(html);
+
+var data = await page.PdfAsync(new PagePdfOptions
+{
+    Format = "A4",
+});
+
+File.WriteAllBytes("abc.pdf", data);
---
+++ src/HtmlToPdf/PuppeteerSharpConverter/Program.cs
@@ -0,0 +1,20 @@
+﻿using PuppeteerSharp;
+
+var httpClient = new HttpClient();
+var response = await httpClient.GetAsync("https://github.com/phongnguyend");
+
+var html = await response.Content.ReadAsStringAsync();
+
+var browserFetcher = new BrowserFetcher();
+await browserFetcher.DownloadAsync();
+
+await using var browser = await Puppeteer.LaunchAsync(new LaunchOptions { Headless = true });
+await using var page = await browser.NewPageAsync();
+await page.SetContentAsync(html);
+
+var data = await page.PdfDataAsync(new PdfOptions
+{
+    PrintBackground = true,
+});
+
+File.WriteAllBytes("abc.pdf", data);
